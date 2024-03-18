8f9eadcc262232c3a1ca514715eddd24440ff446
Joost <2006joost@gmail.com>
2024-03-18 13:57:31 +0100

    added scene and updated wheel script

--- SteamGame/Assets/Scripts/WheelController.cs
+++ SteamGame/Assets/Scripts/WheelController.cs
@@ -10,14 +10,25 @@ public class WheelController : MonoBehaviour
     [SerializeField] WheelCollider RearRight;
     [SerializeField] WheelCollider RearLeft;
 
+    [SerializeField] Transform FrontRightTransform;
+    [SerializeField] Transform FrontLeftTransform;
+    [SerializeField] Transform RearRightTransform;
+    [SerializeField] Transform RearLeftTransform;
+
+
     public float acceleration = 500f;
     public float brakeForce = 300f;
+    private float maxTurnangle = 15f;
 
     private float currentAcceleration = 0f;
     private float currentBrakeForce = 0f;
+    private float currentTurnangle = 0f;
 
     private void FixedUpdate()
     {
+
+        currentAcceleration = acceleration * Input.GetAxis("Vertical");
+        
         if (Input.GetKey(KeyCode.Space))
         {
             currentBrakeForce = brakeForce;
@@ -34,5 +45,23 @@ public class WheelController : MonoBehaviour
         FrontLeft.brakeTorque = currentBrakeForce;
         RearRight.brakeTorque = currentBrakeForce;
         RearLeft.brakeTorque = currentBrakeForce;
+
+        currentTurnangle = maxTurnangle * Input.GetAxis("Horizontal");
+        FrontLeft.steerAngle = currentTurnangle;
+        FrontRight.steerAngle = currentTurnangle;
+        UpdateWheel(FrontLeft, FrontLeftTransform);
+        UpdateWheel(FrontRight, FrontRightTransform);
+        UpdateWheel(RearLeft, RearLeftTransform);
+        UpdateWheel(RearRight, RearRightTransform);
     }
+    private void UpdateWheel(WheelCollider col, Transform trans)
+    {
+        Vector3 position;
+        Quaternion rotation;
+        col.GetWorldPose(out position, out rotation);
+
+        trans.position = position;
+        trans.rotation = rotation;
+    }
+
 }
